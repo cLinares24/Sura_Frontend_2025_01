10ce2fcd8de0d63723ad0e6128b5347be1b22be4
clinaresm <camilo.linaresm@autonoma.edu.co>
2025-12-03 12:21:28 -0500

    Integration with backend production

--- libs/citasService.ts
+++ libs/citasService.ts
@@ -1,29 +1,3 @@
-// import { apiFetch } from "./singletonFetch";
-// import { CrearCitaOutput, ReprogramarCitaInput } from "@/schemas/citas";
-
-
-// export const getCitasDisponiblesService = (id_especialidad: number, fecha: string) => {
-//   return apiFetch(`/citas/disponibles/${id_especialidad}?fecha=${fecha}`, "GET");
-// };
-
-// export const crearCitaService = (body: CrearCitaOutput) => {
-//   return apiFetch('/citas/', "POST", body);
-// };
-
-// export const eliminarCitaService = (id_cita: number) => {
-//   return apiFetch(`/citas/citas/${id_cita}`, "DELETE");
-// };
-
-// export const reprogramarCitaService = (id_cita: number, body: ReprogramarCitaInput) => {
-//   return apiFetch(`/citas/citas/${id_cita}/reprogramar`, "PUT", body);
-// };
-
-// export const confirmarCitaService = (id_cita: number) => {
-//   return apiFetch(`/citas/citas/${id_cita}/confirmar`, "PUT");
-// };
-
-
-// src/libs/citasService.ts
 import { apiFetch } from "./singletonFetch";
 import { CrearCitaOutput, ReprogramarCitaInput } from "@/schemas/citas";
 
